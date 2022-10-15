568277c74a6bebae45ce67eb8bfe6e96459324a4
Yaroslav <perec-polosatb1i@list.ru>
2022-10-15 12:14:19 +0400

    a task 47

---
+++ task_47/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+double[,] generate2dArray(int height, int width, double minValue, double maxValue, int myDecimal)
+{
+    double[,] twoDArray = new double[height, width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Random random = new Random();
+            double temp = Math.Round(random.NextDouble() * (maxValue - minValue) + minValue, myDecimal);
+            twoDArray[i, j] = temp;
+        }
+    }
+    return twoDArray;
+}
+void printColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.Write(data);
+    Console.ResetColor();
+}
+void print2dArray(double[,] arrayToPrint)
+{
+    Console.Write("\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        printColorData(i + "\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        printColorData(i + "\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] generatedArray = generate2dArray(3, 4, -10, 10, 1);
+print2dArray(generatedArray);
